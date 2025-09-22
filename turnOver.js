cffb400af51186cc97b689cc245b233ee999d5ad
Hayk-Tumasyan <hayk.tumasyan2004@gmail.com>
2025-09-23 00:07:33 +0400

    Update turnOver.js

--- turnOver.js
+++ turnOver.js
@@ -1,12 +1,13 @@
 function turnOver(a){
     let b = 0;
     let count = a.toString().length;
-    while(count>=0){
+    while(a>0){
         count--;
         b += a%10 * 10**count;
         a = parseInt(a/10);
     }
     return b;
 }
+
 
 console.log(turnOver(123));
